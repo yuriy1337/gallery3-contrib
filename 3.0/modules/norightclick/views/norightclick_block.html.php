5cf3ded23a8a3ffdb82954bd67c962c87cd63930
Ubuntu <bridalka@brildalkatest.brildalkatest6880.d4.internal.cloudapp.net>
2017-01-19 06:37:04 +0000

    adding norightclick module

---
+++ 3.0/modules/norightclick/views/norightclick_block.html.php
@@ -0,0 +1,22 @@
+<?php defined("SYSPATH") or die("No direct script access.") ?>
+<script language="JavaScript1.1"><!--
+var message="";
+
+function clickIE()
+ 
+{if (document.all)
+{(message);return false;}}
+ 
+function clickNS(e) {
+if
+(document.layers||(document.getElementById&&!document.all))
+{
+if (e.which==2||e.which==3) {(message);return false;}}}
+if (document.layers)
+{document.captureEvents(Event.MOUSEDOWN);document.  onmousedown=clickNS;}
+else
+{document.onmouseup=clickNS;document.oncontextmenu  =clickIE;}
+ 
+document.oncontextmenu=new Function("return false")
+
+//--></script>
